fbc1a528edca8cb91c750070026196cfcc3c25d9
Peter Marsh <p.marsh@mac.com>
2018-05-23 16:32:08 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -34,39 +34,8 @@ const Card = (props) => (
 const App = () => (
     <CardGroup>
         <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-thumbs-o-up card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Trial
-                </div>
-            </div>
-            <div className="card__price">Free!</div>
-        </div> */}
         <Card description="Basic tier" icon="fa-trophy" price="$10.00" hint="(most popular)" />
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-            <div className="icon fa fa-trophy card__descriptionIcon" />
-            <div className="card__descriptionText">
-            Basic tier
-            <br/>
-            (most popular)
-            </div>
-            </div>
-            <div className="card__price">$10.00</div>
-        </div> */}
         <Card description="Advanced tier" icon="fa-bolt" price="$6,000.00" hint="(only for enterprise-level professionals)"  />
-        {/* <div className="card cardGroup__card">
-            <div className="card__description cardGroup__cardDescription">
-                <div className="icon fa fa-bolt card__descriptionIcon" />
-                <div className="card__descriptionText">
-                    Advanced tier
-                    <br/>
-                    (only for enterprise-level professionals)
-                </div>
-            </div>
-            <div className="card__price">$6,000.00</div>
-        </div> */}
     </CardGroup>
 );
 
