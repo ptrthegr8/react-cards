27527f2812ff805b1b92e3e6abfec39ba8065d14
Peter Marsh <p.marsh@mac.com>
2018-05-23 16:34:58 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-/*
- * CardGroup
- * This component takes Card components as children and renders them.
- * Example usage:
- *
- *      <CardGroup>
- *          <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
- *      </CardGroup>
- *
- * The above would render a card group with a single child in it.
- *
- */
 const CardGroup = (props) => (
     <div className="cardGroup">
         {props.children}
